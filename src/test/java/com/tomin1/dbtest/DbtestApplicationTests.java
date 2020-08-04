aa8d817b1c685fde239cdcb73211c9eef06b44fc
LAPTOP-R856RN2V\杨纪超 <502862188@qq.com>
2020-08-04 14:46:04 +0800

    创建git springboot测试项目

---
+++ src/test/java/com/tomin1/dbtest/DbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomin1.dbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
